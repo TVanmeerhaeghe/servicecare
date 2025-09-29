fd915aca1f1e409d433e39706f882b9281ce79d3
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2025-09-29 11:24:54 +0200

    chore: :tada:

---
+++ backend/src/main/java/com/teo/servicecare/ServiceCareApplication.java
@@ -0,0 +1,11 @@
+package com.teo.servicecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCareApplication {
+  public static void main(String[] args) { 
+    SpringApplication.run(ServiceCareApplication.class, args);
+  }
+}
